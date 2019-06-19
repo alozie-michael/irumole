3623fb28dd5ff7d70428b04f6949e92c2c00cc8a
Michael Alozie <alozienedu@gmail.com>
2019-06-19 14:58:19 +0100

    excluded user/register from authorization endpoints

--- src/main/java/com/irumole/ng/config/SecurityConfig.java
+++ src/main/java/com/irumole/ng/config/SecurityConfig.java
@@ -35,7 +35,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
         auth.userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder());
     }
@@ -53,6 +52,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
-                .antMatchers("/user/signUp");
+                .antMatchers("/user/register");
     }
 }
