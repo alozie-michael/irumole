8cb490545ae24b4a86173bcb11d1dff992498e18
Michael Alozie <alozienedu@gmail.com>
2019-07-27 19:40:04 +0100

    cors enable

--- src/main/java/com/irumole/ng/controller/UserController.java
+++ src/main/java/com/irumole/ng/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping(path = "/user")
 public class UserController {
@@ -46,7 +47,6 @@ public class UserController {
         return ResponseEntity.ok(getResponse(userService.signUp(user)));
     }
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping(path = "/bank")
     public ResponseEntity<GenericServiceResponse> getBank(){
         return ResponseEntity.ok(GenericServiceResponseBuilder.aGenericServiceResponseBuilder()
